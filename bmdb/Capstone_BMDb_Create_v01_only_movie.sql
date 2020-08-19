e7de8c04e7f9c1b094875f58f1067920ac7e9141
Dana-Kimmel <dear.prudence34@gmail.com>
2020-08-19 16:26:25 -0400

    added-new-movies

--- bmdb/Capstone_BMDb_Create_v01_only_movie.sql
+++ bmdb/Capstone_BMDb_Create_v01_only_movie.sql
@@ -19,4 +19,12 @@ Director 	varchar(255) 	not null
  	(1, 'Star Wars', 1977, 'PG', 'George Lucas'),
  	(2, 'Sixteen Candles', 1984, 'PG', 'John Hughes'),
   	(3, 'Fifth Element', 1997, 'PG-13', 'Luc Besson'),
- 	(4, 'Better Off Dead', 1985, 'PG', 'Savage Steve Holland');
+ 	(4, 'Better Off Dead', 1985, 'PG', 'Savage Steve Holland'),
+    (5, 'Blazing Saddles', 1974, 'R', 'Mel Brooks'),
+    (6, 'The Evil Dead', 1981, 'NC-17', 'Sam Raimi'),
+    (7, 'The Shining', 1980, 'R', 'Stanley Kubrick'),
+    (8, 'The Return of the Living Dead', 1985, 'R', 'Dan OBannan'),
+    (9, 'Shaun of the Dead', 2004, 'R', 'Edgar Wright')
+    ; 
+    
+    ;
