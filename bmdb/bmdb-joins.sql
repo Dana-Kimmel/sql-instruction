053e574b72df78a34c69768b583b098be253b909
Dana-Kimmel <dear.prudence34@gmail.com>
2020-08-26 13:23:43 -0400

    added-practice-joins

--- bmdb/bmdb-joins.sql
+++ bmdb/bmdb-joins.sql
@@ -6,6 +6,15 @@ select m.id, title, c.id
 role from movie as m
   join credit as c
     on m.id = c.MovieID;
+
+-- inner join: data in common between 3 tables, practice
+select m.title, a.firstname, a.lastname, a.gender
+  from movie as m
+  join credit as c
+    on c.MovieID = m.id
+    join actor as a
+     on c.actorid = a.id;
+     
     
     -- inner join credit - actor 
     select * from credit c
@@ -15,6 +24,16 @@ role from movie as m
       -- outer (left)  join, all movies and actors assigned
       select * from movie
        left join credit
-        on movie.id = credit.MovieID
-    
+        on movie.id = credit.MovieID;
+        
+        
+        -- practice join
+        select g.name, m.title, m.rating
+        from genre as g 
+        join moviegenre as mg 
+        on g.id = mg.genreid
+        join movie as m
+        on mg.movieid = m.id;
+        
+        select * from genre;
     
